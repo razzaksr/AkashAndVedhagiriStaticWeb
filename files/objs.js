a3e421dff9ea1cdc2f62cfd9d72e7cd17b5eb864
razzaksr <razzaksr@gmail.com>
2022-07-19 12:03:25 +0530

    CRUDL done

--- files/objs.js
+++ files/objs.js
@@ -1,6 +1,4 @@
-class Dynamic{
-    constructor(){
-        this.arr=[
+var arr=[
             {
                 "model":"realme5s",
                 "price":13000,
@@ -20,16 +18,15 @@ class Dynamic{
                 "size":5.2
             }
         ]
-    }
 
-    delete(pos){
-        var x=this.arr[pos]
-        this.arr.splice(pos,1)
+    const deletes=(pos)=>{
+        var x=arr[pos]
+        arr.splice(pos,1)
         alert(JSON.stringify(x)+" has been deleted")
-        this.show()
+        show()
     }
 
-    create(){
+    const create=()=>{
         let obj={"model":"","price":0,"ram":0,"size":0.0}
         
         obj.model=prompt("Enter the mobile model ","")
@@ -37,27 +34,32 @@ class Dynamic{
         obj.ram=prompt("Enter the mobile ram ","")
         obj.size=prompt("Enter the mobile size ","")
 
-        this.arr.push(obj)
+        arr.push(obj)
 
         alert(JSON.stringify(obj)+" has pushed")
     }
 
-    show(){
+    const show=()=>{
         var place=document.getElementById('where')
         var content=""
-        this.arr.map((ele,ind)=>{
+        arr.map((ele,ind)=>{
             content+="<tr>"
             content+="<td>"+ele.model+"</td>"
             content+="<td>"+ele.price+"</td>"
             content+="<td>"+ele.ram+"</td>"
             content+="<td>"+ele.size+"</td>"
-            //content+="<td><button onclick='alert("+'+ele.model+'+")'>Delete</button></td>"
-            //content+="<td><button onclick="+"alert('"+ele.model+"')>Delete</button></td>"
-            //content+="<td><button onclick="+"alert('hai')"+">Delete</button></td>"
-            //content+="<td><button onclick="+"this.delete('"+ind+"')"+">Delete</button></td>"
+            content+="<td><button onclick=deletes('"+ind+"')>Delete</button></td>"
+            content+="<td><button onclick=edit('"+ind+"')>Edit</button></td>"
             content+="</tr>"
         })
-
         place.innerHTML=content
     }
-}
+
+    const edit=(pos)=>{
+        arr[pos].model=prompt("Enter the new model instead of "+arr[pos].model,"")
+        arr[pos].price=prompt("Enter the new price instead of "+arr[pos].price,"")
+        arr[pos].ram=prompt("Enter the new ram instead of "+arr[pos].ram,"")
+        arr[pos].size=prompt("Enter the new size instead of "+arr[pos].size,"")
+
+        show()
+    }
